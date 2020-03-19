7c44b7ef16dee8a9fd834cb45b410460c824c1cc
linqingping <linqingping@users.noreply.github.com>
2020-03-19 10:39:19 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,12 @@ from .celery import celery_app
 
 def create_app():
     app = Flask(__name__)
-
+    
     celery_app.conf.update({"broker_url": 'redis://127.0.0.1:6379/0',
                             "result_backend": 'redis://127.0.0.1:6379/0', })
+    # 导入创建的蓝图
     from .urls import assets_page
-
+    # 注册这个蓝图对象
     app.register_blueprint(assets_page)
     return app
 
