2a7c8f3adcf521aadea4284d767db1673d8201f4
linqingping <linqingping@users.noreply.github.com>
2020-03-19 10:34:14 +0800

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from flask_restful import Resource, Api
-from .server import Servers, Server,Taskstatus
+from .server import Servers, Server,Taskstatus #导入路由的具体实现方法
 from flask import Blueprint
 
+#创建一个蓝图对象
 assets_page = Blueprint('assets_page', __name__)
+#在这个蓝图对象上进行操作,注册路由
 api = Api(assets_page)
 
 #注册路由
