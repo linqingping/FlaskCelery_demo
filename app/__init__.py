67b3d389dbb957e1a91532b9fd03294c2e187492
linqingping <linqingping@users.noreply.github.com>
2020-03-31 15:35:07 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from .celery import celery_app
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
+    
+    #CORS跨域：FLask下如何解决跨域问题
+    CORS(app) 
     
     celery_app.conf.update({"broker_url": 'redis://127.0.0.1:6379/0',
                             "result_backend": 'redis://127.0.0.1:6379/0', })
